c7051e7b5f37eea030ce821e9c7541a5de6b26a7
Casey Dwyer <caseydwyer@gmail.com>
2012-01-01 00:42:34 -0800

    Improved controls.

--- src/main.c
+++ src/main.c
@@ -6,29 +6,60 @@
 #define SCREEN_H 240
 #define SCREEN_BPP 32
 
+#define CAMERA_SPEED 2
+
+typedef struct {
+    int up;
+    int down;
+    int left;
+    int right;
+} Controls;
+
 int game(TMP_Tilemap * tilemap, SDL_Surface * screen)
 {
     SDL_Rect camera = { 0, 0, SCREEN_W, SCREEN_H };
     SDL_Event event;
+    Controls controller = { 0, 0, 0, 0 };
     int i;
 
     while (1) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_KEYDOWN) {
                 if (event.key.keysym.sym == SDLK_LEFT) {
-                    camera.x--;
+                    controller.left = 1;
                 } else if (event.key.keysym.sym == SDLK_RIGHT) {
-                    camera.x++;
+                    controller.right = 1;
                 } else if (event.key.keysym.sym == SDLK_UP) {
-                    camera.y--;
+                    controller.up = 1;
                 } else if (event.key.keysym.sym == SDLK_DOWN) {
-                    camera.y++;
+                    controller.down = 1;
+                } else if (event.key.keysym.sym == SDLK_q) {
+                    return 0;
+                }
+            } else if (event.type == SDL_KEYUP) {
+                if (event.key.keysym.sym == SDLK_LEFT) {
+                    controller.left = 0;
+                } else if (event.key.keysym.sym == SDLK_RIGHT) {
+                    controller.right = 0;
+                } else if (event.key.keysym.sym == SDLK_UP) {
+                    controller.up = 0;
+                } else if (event.key.keysym.sym == SDLK_DOWN) {
+                    controller.down = 0;
                 } else if (event.key.keysym.sym == SDLK_q) {
                     return 0;
                 }
             } else if (event.type == SDL_QUIT) {
                 return 0;
             }
+        }
+        if (controller.left) {
+            camera.x -= CAMERA_SPEED;
+        } else if (controller.right) {
+            camera.x += CAMERA_SPEED;
+        } else if (controller.up) {
+            camera.y -= CAMERA_SPEED;
+        } else if (controller.down) {
+            camera.y += CAMERA_SPEED;
         }
         for (i = 0; i < tilemap->depth; i++) {
             if (SDL_BlitSurface(tilemap->layers[i], &camera, screen, NULL)) {
