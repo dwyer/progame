a944b3b938216bed9c4815b5cd8a72de445a6ab5
Xunie <xunie@anapnea.net>
2012-01-03 05:50:43 +0100

    Fixed error reporting \n bug and added an input base.

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "SDL/SDL.h"
 #include "main.h"
 #include "world.h"
+#include "input.h"
 
 #define SPEEDPPS 0.2
 
@@ -23,13 +24,13 @@ int main(int argc, char *argv[])
 	unsigned int StartTime = 0, CurrentTime = 0;
 
 	if (SDL_Init(SDL_INIT_EVERYTHING)) {
-		fputs(SDL_GetError(), stderr);
+		fprintf(stderr, "%s\n", SDL_GetError());
 		return -1;
 	}
 	if ((screen =
 		 SDL_SetVideoMode(SCREEN_W, SCREEN_H, SCREEN_BPP,
 						  SDL_HWSURFACE)) == NULL) {
-		fputs(SDL_GetError(), stderr);
+		fprintf(stderr, "%s\n", SDL_GetError());
 		return -1;
 	}
 	if ((world = createWorld(filename)) == NULL) {
@@ -39,14 +40,16 @@ int main(int argc, char *argv[])
 	 * linked-list of all entities (the player, npcs, enemies, items, etc.) */
 	while (updateWorld(world, CurrentDelay)) {
 		StartTime = SDL_GetTicks();
+		ProcessInput();
+
 		/* Draw. */
 		if (drawWorld(world, screen)) {
-			fputs(SDL_GetError(), stderr);
+			fprintf(stderr, "%s\n", SDL_GetError());
 			return -1;
 		}
 		/* Update screen. */
 		if (SDL_Flip(screen) == -1) {
-			fputs(SDL_GetError(), stderr);
+			fprintf(stderr, "%s\n", SDL_GetError());
 			return -1;
 		}
 		GetDelay(&CurrentDelay, &AverageDelay, StartTime);
