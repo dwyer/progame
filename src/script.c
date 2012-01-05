58d3891f150d7c8438497d1c788324688f3d03bb
Casey Dwyer <caseydwyer@gmail.com>
2012-01-04 18:46:09 -0800

    Still working on it. Removed annoying debug messages.

--- src/script.c
+++ src/script.c
@@ -63,6 +63,11 @@ void registerLuaRegs(lua_State *L) {
 	}
 }
 
+/**
+ * Loads the map. Takes one argument, which should be the Lua table exported by
+ * Tiled's Lua plugin. Call with `load_map(dofile(filename))` where filename is
+ * the name of the file exported by tiled.
+ */
 int load_map(lua_State *L) {
 	Tilemap *tmp = malloc(sizeof(Tilemap));
 	int t = 1;
@@ -72,7 +77,9 @@ int load_map(lua_State *L) {
 		const char *key = lua_tostring(L, -2);
 		const char *type = lua_typename(L, lua_type(L, -1));
 		
-		printf("%s - %s\n", key, type);
+#ifdef DEBUG
+		printf("%s (%s)\n", key, type);
+#endif
 		if (!strcmp(key, "width"))
 			tmp->width = lua_tonumber(L, -1);
 		else if (!strcmp(key, "height"))
@@ -82,7 +89,25 @@ int load_map(lua_State *L) {
 		else if (!strcmp(key, "tileheight"))
 			tmp->tileheight = lua_tonumber(L, -1);
 		else if (!strcmp(key, "layers")) {
-			/* keep walking... */
+			int i, n = lua_objlen(L, -1);
+			tmp->layers = calloc(n, sizeof(TilemapLayer));
+			for (i = 0; i < n; i++) {
+#ifdef DEBUG
+				printf("* Layer %d\n", i);
+#endif
+				lua_pushvalue(L, -1);
+				lua_pushnil(L);
+				while (lua_next(L, 1)) {
+					key = lua_tostring(L, -2);
+					type = lua_typename(L, lua_type(L, -1));
+#ifdef DEBUG
+					printf("  %s (%s)\n", key, type);
+#endif
+					lua_pop(L, 1);
+				}
+				lua_pop(L, 1);
+			}
+			free(tmp->layers); /* remove this when this function is complete */
 		}
 		lua_pop(L, 1);
 	}
@@ -92,6 +117,7 @@ int load_map(lua_State *L) {
 			tmp->tilewidth,
 			tmp->tileheight
 			);
+	free(tmp); /* remove this when the code does something */
 	return 1;
 }
 
