66a542867b48a91579f7f7b8c4a69fc3e0a8586e
Casey Dwyer <caseydwyer@gmail.com>
2012-01-16 19:58:06 -0800

    Tuning.

--- src/entity.c
+++ src/entity.c
@@ -3,6 +3,12 @@
 #include "main.h"
 
 enum {
+	ACTION_STANDING,
+	ACTION_WALKING,
+	NUM_ACTIONS
+};
+
+enum {
 	DIRECTION_UP,
 	DIRECTION_DOWN,
 	DIRECTION_LEFT,
@@ -10,24 +16,14 @@ enum {
 	NUM_DIRECTIONS
 };
 
-enum {
-	ACTION_LOOKING,
-	ACTION_WALKING,
-	NUM_ACTIONS
-};
-
 typedef struct FrameSet FrameSet;
 
+void Entity_add_frame(Entity * entity, int action, int direction, SDL_Rect *frame);
+
 struct FrameSet {
-	SDL_Rect *frames;
+	SDL_Rect **frames;
 	int length;
 };
-
-void FrameSet_add_frame(FrameSet *frameset, SDL_Rect frame) {
-	frameset->frames = realloc(frameset->frames, (frameset->length + 1) * sizeof(*frameset->frames));
-	frameset->frames[frameset->length] = frame;
-	frameset->length++;
-}
 
 struct Entity {
 	/** Position of the entity relative to the current world map, NOT the
@@ -38,8 +34,8 @@ struct Entity {
 	SDL_Rect vel;
 
 	SDL_Surface *sprite;
-	FrameSet frames[NUM_ACTIONS][NUM_DIRECTIONS];
-	unsigned int frameset;
+	SDL_Rect *frames;
+	FrameSet framesets[NUM_ACTIONS][NUM_DIRECTIONS];
 	unsigned int frame;
 	unsigned int action;
 	unsigned int direction;
@@ -62,7 +58,6 @@ Entity *Entity_new() {
 }
 
 int Entity_set_sprite(Entity *entity, const char *filename) {
-	SDL_Rect *frames;
 	int i, j, w, h;
 
 	if ((entity->sprite = SDL_LoadBMP(filename)) == NULL)
@@ -70,80 +65,59 @@ int Entity_set_sprite(Entity *entity, const char *filename) {
 	SDL_SetColorKey(entity->sprite, SDL_SRCCOLORKEY, 0xff00ff);
 	w = entity->sprite->w / entity->pos.w;
 	h = entity->sprite->h / entity->pos.h;
-	frames = calloc(w * h, sizeof(*frames));
+	entity->frames = calloc(w * h, sizeof(*entity->frames));
 	for (i = 0; i < w; i++)
 		for (j = 0; j < h; j++) {
-			frames[i + j * w].x = i * entity->pos.w;
-			frames[i + j * w].y = j * entity->pos.h;
-			frames[i + j * w].w = entity->pos.w;
-			frames[i + j * w].h = entity->pos.h;
+			entity->frames[i + j * w].x = i * entity->pos.w;
+			entity->frames[i + j * w].y = j * entity->pos.h;
+			entity->frames[i + j * w].w = entity->pos.w;
+			entity->frames[i + j * w].h = entity->pos.h;
 		}
-	/* Bind frames to states. Hardcoded for now. TODO: do this in lua. */
 	entity->frame = 0;
-	entity->action = ACTION_LOOKING;
+	entity->action = ACTION_STANDING;
 	entity->direction = DIRECTION_DOWN;
 	for (i = 0; i < NUM_ACTIONS; i++)
 		for (j = 0; j < NUM_DIRECTIONS; j++) {
-			entity->frames[i][j].frames = NULL;
-			entity->frames[i][j].length = 0;
+			entity->framesets[i][j].frames = NULL;
+			entity->framesets[i][j].length = 0;
 		}
-	FrameSet_add_frame(&entity->frames[ACTION_WALKING][DIRECTION_UP], frames[0]);
-	FrameSet_add_frame(&entity->frames[ACTION_LOOKING][DIRECTION_UP], frames[1]);
-	FrameSet_add_frame(&entity->frames[ACTION_WALKING][DIRECTION_UP], frames[2]);
-	FrameSet_add_frame(&entity->frames[ACTION_WALKING][DIRECTION_RIGHT], frames[3]);
-	FrameSet_add_frame(&entity->frames[ACTION_LOOKING][DIRECTION_RIGHT], frames[4]);
-	FrameSet_add_frame(&entity->frames[ACTION_WALKING][DIRECTION_RIGHT], frames[5]);
-	FrameSet_add_frame(&entity->frames[ACTION_WALKING][DIRECTION_DOWN], frames[6]);
-	FrameSet_add_frame(&entity->frames[ACTION_LOOKING][DIRECTION_DOWN], frames[7]);
-	FrameSet_add_frame(&entity->frames[ACTION_WALKING][DIRECTION_DOWN], frames[8]);
-	FrameSet_add_frame(&entity->frames[ACTION_WALKING][DIRECTION_LEFT], frames[9]);
-	FrameSet_add_frame(&entity->frames[ACTION_LOOKING][DIRECTION_LEFT], frames[10]);
-	FrameSet_add_frame(&entity->frames[ACTION_WALKING][DIRECTION_LEFT], frames[11]);
+	/* Bind frames to states. Hardcoded for now. TODO: do this in lua. */
+	Entity_add_frame(entity, ACTION_WALKING, DIRECTION_UP, &entity->frames[0]);
+	Entity_add_frame(entity, ACTION_STANDING, DIRECTION_UP, &entity->frames[1]);
+	Entity_add_frame(entity, ACTION_WALKING, DIRECTION_UP, &entity->frames[2]);
+	Entity_add_frame(entity, ACTION_WALKING, DIRECTION_RIGHT, &entity->frames[3]);
+	Entity_add_frame(entity, ACTION_STANDING, DIRECTION_RIGHT, &entity->frames[4]);
+	Entity_add_frame(entity, ACTION_WALKING, DIRECTION_RIGHT, &entity->frames[5]);
+	Entity_add_frame(entity, ACTION_WALKING, DIRECTION_DOWN, &entity->frames[6]);
+	Entity_add_frame(entity, ACTION_STANDING, DIRECTION_DOWN, &entity->frames[7]);
+	Entity_add_frame(entity, ACTION_WALKING, DIRECTION_DOWN, &entity->frames[8]);
+	Entity_add_frame(entity, ACTION_WALKING, DIRECTION_LEFT, &entity->frames[9]);
+	Entity_add_frame(entity, ACTION_STANDING, DIRECTION_LEFT, &entity->frames[10]);
+	Entity_add_frame(entity, ACTION_WALKING, DIRECTION_LEFT, &entity->frames[11]);
 	return 0;
 }
 
 /**
- * Free's an entity from memory.
+ * Free an entity from memory.
  */
 void Entity_free(Entity * entity) {
+	int i, j;
+
+	SDL_FreeSurface(entity->sprite);
+	for (i = 0; i < NUM_ACTIONS; i++)
+		for (j = 0; j < NUM_DIRECTIONS; j++)
+			free(entity->framesets[i][j].frames);
+	free(entity->frames);
 	free(entity);
 }
 
-SDL_Rect Entity_get_pos(const Entity * entity) {
-	return entity->pos;
-}
+void Entity_add_frame(Entity * entity, int action, int direction, SDL_Rect *frame) {
+	FrameSet *frameset = &entity->framesets[action][direction];
 
-SDL_Rect Entity_get_vel(const Entity * entity) {
-	return entity->vel;
-}
-
-/**
- * Set the position of the given Entity to the given x and y.
- */
-void Entity_set_pos(Entity * entity, int x, int y) {
-	entity->pos.x = x;
-	entity->pos.y = y;
-}
-
-void Entity_set_vel(Entity * entity, int x, int y) {
-	if (x || y)
-		entity->action = ACTION_WALKING;
-	else
-		entity->action = ACTION_LOOKING;
-	if (x < 0)
-		entity->direction = DIRECTION_LEFT;
-	else if (x > 0)
-		entity->direction = DIRECTION_RIGHT;
-	if (y < 0)
-		entity->direction = DIRECTION_UP;
-	else if (y > 0)
-		entity->direction = DIRECTION_DOWN;
-	if (x == entity->vel.x && y == entity->vel.y)
-		entity->frame++;
-	else
-		entity->frame = 0;
-	entity->vel.x = x;
-	entity->vel.y = y;
+	frameset->frames = realloc(frameset->frames,
+							   (frameset->length + 1) * sizeof(*frameset->frames));
+	frameset->frames[frameset->length] = frame;
+	frameset->length++;
 }
 
 /**
@@ -155,14 +129,60 @@ void Entity_set_size(Entity * entity, int w, int h) {
 }
 
 /**
+ * Set the position of the given Entity to the given x and y.
+ */
+void Entity_set_pos(Entity * entity, int x, int y) {
+	entity->pos.x = x;
+	entity->pos.y = y;
+}
+
+/**
+ * Set the given entity's velocity and adjust their animation frame
+ * accordingly.
+ */
+void Entity_set_vel(Entity * entity, int x, int y) {
+	/* set action */
+	if (x || y)
+		entity->action = ACTION_WALKING;
+	else
+		entity->action = ACTION_STANDING;
+	/* set direction */
+	if (x < 0)
+		entity->direction = DIRECTION_LEFT;
+	else if (x > 0)
+		entity->direction = DIRECTION_RIGHT;
+	if (y < 0)
+		entity->direction = DIRECTION_UP;
+	else if (y > 0)
+		entity->direction = DIRECTION_DOWN;
+	/* set frame */
+	if (x == entity->vel.x && y == entity->vel.y)
+		entity->frame++;
+	else
+		entity->frame = 0;
+	/* set velocity */
+	entity->vel.x = x;
+	entity->vel.y = y;
+}
+
+SDL_Rect Entity_get_pos(const Entity * entity) {
+	return entity->pos;
+}
+
+SDL_Rect Entity_get_vel(const Entity * entity) {
+	return entity->vel;
+}
+
+/**
  * Draw the entity if and only if the entity is within the bounds of the
  * camera.
  * \return 0 on success, non-zero on error.
  */
 int Entity_draw(Entity * entity, SDL_Surface * screen, SDL_Rect camera) {
-	FrameSet frameset = entity->frames[entity->action][entity->direction];
-	SDL_Rect src = frameset.frames[entity->frame % frameset.length];
+	FrameSet frameset = entity->framesets[entity->action][entity->direction];
+	SDL_Rect src = *frameset.frames[entity->frame % frameset.length];
 	SDL_Rect dst = entity->pos;
+
 	dst.x -= camera.x;
 	dst.y -= camera.y;
 	if ((dst.x + dst.w >= 0 && dst.x < screen->w) &&
